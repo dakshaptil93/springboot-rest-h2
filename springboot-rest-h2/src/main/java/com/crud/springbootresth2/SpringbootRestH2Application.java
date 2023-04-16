8a8f8238c4ad5a0970a4ff00302f4610b4495b3b
dakshaptil93 <53445877+dakshaptil93@users.noreply.github.com>
2023-04-16 18:04:49 +0530

    Add files via upload
    Initial Setup

---
+++ springboot-rest-h2/src/main/java/com/crud/springbootresth2/SpringbootRestH2Application.java
@@ -0,0 +1,13 @@
+package com.crud.springbootresth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestH2Application.class, args);
+	}
+
+}
---
+++ springboot-rest-h2/src/test/java/com/crud/springbootresth2/SpringbootRestH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.springbootresth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
